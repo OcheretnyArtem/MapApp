9e02f4d5e7533244f4dbef1f29e1a1aa03c91515
artsiomacharetny <ocheretnyartem@gmail.com>
2025-01-30 12:56:37 +0100

    Create LocationManager and move location observing logic from Ui

---
+++ app/src/main/java/pl/artoch/maps_app/LocationManager.kt
@@ -0,0 +1,49 @@
+package pl.artoch.maps_app
+
+import android.content.Context
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.os.Looper
+import androidx.core.app.ActivityCompat.checkSelfPermission
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
+
+interface LocationManager {
+
+    fun requestLocationUpdates(onNewLocation: (LatLng) -> Unit)
+
+    fun stopLocationUpdates()
+
+}
+
+class LocationManagerImpl(
+    private val locationSource: LocationSource = LocationSource,
+    private val context: Context
+) : LocationManager {
+
+    private var onNewLocation: ((LatLng) -> Unit)? = null
+    private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            locationResult.locations.forEach { location ->
+                locationSource.onNewLocation(location)
+                onNewLocation?.invoke(LatLng(location.latitude, location.longitude))
+            }
+        }
+    }
+
+    override fun requestLocationUpdates(onNewLocation: (LatLng) -> Unit) {
+        if (Permissions.mapPermissions().all { checkSelfPermission(context, it) == PERMISSION_GRANTED }) {
+            val locationRequest = LocationRequest.Builder(1000L).build()
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+            this.onNewLocation = onNewLocation
+        }
+    }
+
+    override fun stopLocationUpdates() {
+        locationSource.deactivate()
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+}
